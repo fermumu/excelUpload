eca77e7fe573097dc3de4d7b62a83bce401a4f8c
Fernando Muñoz <fernando1986caballeria@gmail.com>
2026-01-17 21:13:44 -0500

    first commit

---
+++ src/main/java/com/proyecto/excelupload/ExtraerInformacionDeExcelApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.excelupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExtraerInformacionDeExcelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExtraerInformacionDeExcelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyecto/excelupload/ExtraerInformacionDeExcelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.excelupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtraerInformacionDeExcelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
